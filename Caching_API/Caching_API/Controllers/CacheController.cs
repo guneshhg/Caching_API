052274bb3a5ca1c464f682fd2c20ef24ff02ec7e
RPK\hgunes <h.gunes@krosotomotiv.com.tr>
2021-09-07 12:40:48 +0300

    Caching

---
+++ Caching_API/Caching_API/Controllers/CacheController.cs
@@ -0,0 +1,48 @@
+﻿using Caching_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caching_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+        public CacheController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+       [HttpGet("{key}")]
+       public IActionResult GetCache(string  key)
+        {
+            string value = string.Empty;
+            _memoryCache.TryGetValue(key, out value);
+            return Ok(value);
+        }
+
+
+        [HttpPost]
+        public IActionResult SetCache(CacheRequest data)
+        {
+            var cacheExpiryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(5),
+                Priority = CacheItemPriority.High,
+                SlidingExpiration=TimeSpan.FromMinutes(2),
+                Size = 1024
+            };
+
+            _memoryCache.Set(data.key, data.value, cacheExpiryOptions);
+            return Ok();
+        }
+
+
+    }
+}
---
+++ Caching_API/Caching_API/Startup.cs
@@ -0,0 +1,69 @@
+using Caching_API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caching_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("sqlConnection"),
+                   b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+           
+            
+            
+            services.AddControllers();
+            
+            
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = "localhost:6379";
+            });
+            
+            
+            services.AddMemoryCache();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
